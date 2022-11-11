eac0af5ce70270cd9179d8cfa7224b2dc582bcf0
Jitori <jcoumarb@unca.edu>
2022-11-11 15:36:48 -0500

    adfds

---
+++ quiz02completed - Jahfari/exercise01/main.js
@@ -0,0 +1,32 @@
+function showFox() {
+    // your code here...
+    console.log('Change image and paragraph to fox...');
+    document.getElementById("pic").src="images/fox.jpg";
+    document.getElementById("text").innerText = "Fox";
+   
+}
+
+function showLion() {
+    // your code here...
+    console.log('Change image and paragraph to lion...');
+    document.getElementById("pic").src="images/lion.jpg";
+    document.getElementById("text").innerText = "Lion";
+
+}
+
+function showTiger() {
+    // your code here...
+    console.log('Change image and paragraph to tiger...');
+    document.getElementsByName("p").src="images/tiger.png";
+    document.getElementById("text").innerText = "Tiger";
+
+}
+
+function showZebra() {
+    // your code here...
+    console.log('Change image and paragraph to zebra...');
+    document.getElementById("pic").src="images/zebra.jpg";
+    document.getElementById("text").innerText = "Zebra";
+
+}
+
